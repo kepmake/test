2275e66042367ce3bdc6583cf6a04e2575925aeb
Igor <kepmake@gmail.com>
2021-08-03 16:51:25 +0300

    ng1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-window.onkeydown = e=>e.key==' ' ? cs.innerText++:0;
+
+
+const ngb=document.querySelector(".new_game_button");
+ngb.onclick = function(){
+    ngb.style.display = 'none';
+    document.body.style.background = '#000000';
+  };
+
